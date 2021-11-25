91cff018d3585e1704dedb784619b99f4f684441
THypnosT <zunigaw@uninorte.edu.co>
2021-11-24 22:19:09 -0500

    tabs

--- lib/pagina_contenido/feed.dart
+++ lib/pagina_contenido/feed.dart
@@ -9,18 +9,71 @@ class feed extends StatelessWidget {
     // TODO: implement build
     return MaterialApp(
       title: "Newsooze",
-      home: Inicio(),
-      theme: ThemeData(),
+      home: MyStatefulWidget(),
     );
   }
 }
 
-Widget cuerpo_feed() {
-  return Container(
-    decoration: BoxDecoration(
-        image: DecorationImage(
-            image: NetworkImage(
-                "https://png.pngtree.com/thumb_back/fw800/background/20210429/pngtree-manuscript-effect-vintage-paper-image_676909.jpg"),
-            fit: BoxFit.cover)),
-  );
+/// This is the stateful widget that the main application instantiates.
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+/// AnimationControllers can be created with `vsync: this` because of TickerProviderStateMixin.
+class _MyStatefulWidgetState extends State<MyStatefulWidget>
+    with TickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 4, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Newsooze'),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: const <Widget>[
+            Tab(
+              icon: Icon(Icons.home),
+            ),
+            Tab(
+              icon: Icon(Icons.people),
+            ),
+            Tab(
+              icon: Icon(Icons.notifications),
+            ),
+            Tab(
+              icon: Icon(Icons.supervised_user_circle),
+            ),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: const <Widget>[
+          Center(
+            child: Text("Feed"),
+          ),
+          Center(
+            child: Text("Usuarios"),
+          ),
+          Center(
+            child: Text("Notificaciones"),
+          ),
+          Center(
+            child: Text("Perfil"),
+          ),
+        ],
+      ),
+    );
+  }
 }
